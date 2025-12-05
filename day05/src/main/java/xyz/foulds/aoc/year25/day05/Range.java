97c7bb281fa4fe821b759ab89698577890259dbe
Ryan Foulds <ryanfoulds@live.com>
2025-12-05 12:23:29 +0000

    Improve day 5 solution, sort up-front

--- day05/src/main/java/xyz/foulds/aoc/year25/day05/Range.java
+++ day05/src/main/java/xyz/foulds/aoc/year25/day05/Range.java
@@ -1,7 +1,5 @@
 package xyz.foulds.aoc.year25.day05;
 
-import java.util.Collection;
-
 public class Range {
     private final long start;
     private final long end;
@@ -17,17 +15,17 @@ public class Range {
     public boolean contains(final long test) {
         return test >= start && test <= end;
     }
-
-    public boolean overlaps(final Range other) {
-        return  !(this.end < other.start || this.start > other.end);
-    }
-
-    public Range combineWith(final Collection<Range> others) {
-        return new Range(Math.min(this.start, others.stream().mapToLong(r -> r.start).min().getAsLong()),
-                Math.max(this.end, others.stream().mapToLong(r -> r.end).max().getAsLong()));
-    }
-
     public long size() {
         return this.end - this.start + 1;
+    }
+    public long getStart() {
+        return start;
+    }
+    public long getEnd() {
+        return end;
+    }
+
+    public static Range combine(final Range one, final Range other) {
+        return new Range(Math.min(one.start, other.start), Math.max(one.end, other.end));
     }
 }
--- day05/src/main/java/xyz/foulds/aoc/year25/day05/Ranges.java
+++ day05/src/main/java/xyz/foulds/aoc/year25/day05/Ranges.java
@@ -1,31 +1,34 @@
 package xyz.foulds.aoc.year25.day05;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Stack;
 
 public class Ranges {
-    private final List<Range> ranges;
-    public Ranges(final Collection<Range> inputRanges) {
-        this.ranges = new ArrayList<>(inputRanges.size());
+    private final Stack<Range> ranges;
+    public Ranges(final List<Range> inputRanges) {
+        this.ranges = new Stack<>();
+        inputRanges.sort(Comparator.comparing(Range::getStart));
 
         for (var input : inputRanges) {
-            Collection<Range> overlapsWith = new ArrayList<>();
-            for (var existing : ranges) {
-                if (existing.overlaps(input)) {
-                    overlapsWith.add(existing);
-                }
-            }
-            if (overlapsWith.isEmpty()) {
-                ranges.add(input);
+            if (overlapsLatest(input)) {
+                final var latest = ranges.pop();
+                ranges.push(Range.combine(latest, input));
             } else {
-                ranges.removeAll(overlapsWith);
-                ranges.add(input.combineWith(overlapsWith));
+                ranges.push(input);
             }
         }
     }
 
     public long getCombinedSize() {
         return ranges.stream().mapToLong(Range::size).sum();
+    }
+
+    private boolean overlapsLatest(final Range other) {
+        if (ranges.isEmpty()) {
+            return false;
+        }
+        final var latest = ranges.peek();
+        return other.getStart() <= latest.getEnd();
     }
 }
