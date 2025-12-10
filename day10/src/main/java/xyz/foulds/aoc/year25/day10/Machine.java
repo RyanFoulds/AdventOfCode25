f5afae498a078dc6dddbaf1bdbb11aa59d7f9cba
Ryan Foulds <ryanfoulds@live.com>
2025-12-10 19:07:21 +0000

    do part 1 with bitwise maths on ints

--- day10/src/main/java/xyz/foulds/aoc/year25/day10/Machine.java
+++ day10/src/main/java/xyz/foulds/aoc/year25/day10/Machine.java
@@ -4,33 +4,43 @@ import com.microsoft.z3.Context;
 import com.microsoft.z3.IntExpr;
 import com.microsoft.z3.IntNum;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Queue;
-import java.util.function.Predicate;
 
 public class Machine {
 
+    private static final int[] POW_OF_2 = new int[]{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16_384};
     private final List<List<Integer>> buttons;
+    private final List<Integer> binaryButtons;
     private final boolean[] goalLightState;
     private final long[] joltageRequirements;
+    private final int goalLightBinary;
 
     public Machine(final String line) {
         var sections = line.split(" ");
         final String lights = sections[0].substring(1, sections[0].length() - 1);
         this.goalLightState = new boolean[lights.length()];
+        var binary = 0;
         for (int i = 0; i < this.goalLightState.length; i++) {
             this.goalLightState[i] = lights.charAt(i) == '#';
+            if (this.goalLightState[i]) {
+                binary += Math.powExact(2, goalLightState.length - i - 1);
+            }
         }
+        this.goalLightBinary = binary;
 
-        this. buttons = Arrays.stream(sections, 1, sections.length - 1)
+        this.buttons = Arrays.stream(sections, 1, sections.length - 1)
                 .map(s -> s.substring(1, s.length() - 1))
                 .map(indices -> Arrays.stream(indices.split(","))
                         .map(Integer::parseInt)
                         .toList())
+                .toList();
+
+        this.binaryButtons = buttons.stream()
+                .map(l -> l.stream()
+                        .mapToInt(i -> Math.powExact(2, goalLightState.length - i - 1))
+                        .sum())
                 .toList();
 
         final String joltages = sections[sections.length - 1].substring(1, sections[sections.length - 1].length() - 1);
@@ -40,22 +50,23 @@ public class Machine {
     }
 
     public long findFewestPresses() {
-        final Queue<LightState> queue = new ArrayDeque<>();
-        final HashSet<LightState> visited = new HashSet<>();
-        queue.offer(new LightState(goalLightState.length));
-        visited.add(queue.peek());
+        var minPresses = Long.MAX_VALUE;
 
-        while (!queue.isEmpty()) {
-            final var current = queue.poll();
-            if (current.matchesLights(goalLightState)) {
-                return current.presses();
+        // Just try all possible combinations of button presses, only 0 or 1 each.
+        final int allSet = Math.toIntExact(Math.powExact(2L, buttons.size()));
+        for (int i = 0; i < allSet; i++) {
+            var result = 0;
+            for (int j = 0; j < POW_OF_2.length && j < binaryButtons.size(); j++){
+                if ((i / POW_OF_2[j]) % 2 == 1)  {
+                    var button = binaryButtons.get(j);
+                    result = result ^ button;
+                }
             }
-            buttons.stream()
-                    .map(current::next)
-                    .filter(Predicate.not(visited::contains))
-                    .forEach(queue::offer);
+            if (result == goalLightBinary) {
+                minPresses = Math.min(Integer.bitCount(i), minPresses);
+            }
         }
-        return 0L;
+        return minPresses;
     }
 
     public long findFewestJoltagePresses() {
